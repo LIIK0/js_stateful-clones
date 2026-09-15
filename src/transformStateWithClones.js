f1d6d5beb9f466868967cc01e14bf50381f495a3
LIIK0 <li2002yaroslav@gmail.com>
2026-09-15 19:59:37 +0200

    First attempt

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,37 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const resultTable = [];
+
+  for (const x of actions) {
+    if (x.type === 'addProperties') {
+      if (x.extraData !== null) {
+        for (const i in x.extraData) {
+          stateCopy[i] = x.extraData[i];
+        }
+        resultTable.push(stateCopy);
+      }
+    }
+
+    if (x.type === 'removeProperties') {
+      if (x.keysToRemove.length >= 0) {
+        for (const i of x.keysToRemove) {
+          delete stateCopy[i];
+        }
+        resultTable.push(stateCopy);
+      }
+    }
+
+    if (x.type === 'clear') {
+      for (const i in stateCopy) {
+        delete stateCopy[i];
+      }
+      resultTable.push(stateCopy);
+    }
+  }
+
+  return resultTable;
 }
 
 module.exports = transformStateWithClones;
