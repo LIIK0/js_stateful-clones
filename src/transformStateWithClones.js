baf4bee74983e4b7a356c854ed9b284be9c16254
LIIK0 <li2002yaroslav@gmail.com>
2026-09-17 05:27:56 +0200

    Refactored code

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,41 +11,46 @@ function transformStateWithClones(state, actions) {
   const resultTable = [];
 
   for (const x of actions) {
-    if (x.type === 'addProperties') {
-      if (x.extraData !== null) {
-        for (const i in x.extraData) {
-          stateCopy[i] = x.extraData[i];
+    switch (x.type) {
+      case 'addProperties':
+        if (x.extraData !== null) {
+          for (const i in x.extraData) {
+            stateCopy[i] = x.extraData[i];
+          }
+
+          const newState = { ...stateCopy };
+
+          resultTable.push(newState);
         }
+        break;
 
-        const newState = { ...stateCopy };
+      case 'removeProperties':
+        if (x.type === 'removeProperties') {
+          if (x.keysToRemove.length >= 0) {
+            for (const i of x.keysToRemove) {
+              delete stateCopy[i];
+            }
 
-        resultTable.push(newState);
-      }
-      continue;
-    }
+            const newState = { ...stateCopy };
 
-    if (x.type === 'removeProperties') {
-      if (x.keysToRemove.length >= 0) {
-        for (const i of x.keysToRemove) {
-          delete stateCopy[i];
+            resultTable.push(newState);
+          }
         }
+        break;
 
-        const newState = { ...stateCopy };
+      case 'clear':
+        if (x.type === 'clear') {
+          for (const i in stateCopy) {
+            delete stateCopy[i];
+          }
 
-        resultTable.push(newState);
-      }
-      continue;
-    }
+          const newState = { ...stateCopy };
 
-    if (x.type === 'clear') {
-      for (const i in stateCopy) {
-        delete stateCopy[i];
-      }
-
-      const newState = { ...stateCopy };
-
-      resultTable.push(newState);
-      continue;
+          resultTable.push(newState);
+        }
+        break;
+      default:
+        return 'Problem accured';
     }
   }
 
